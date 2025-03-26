a1784011bd97c06c85b2788ac990622ffbc9d532
Mina Ayman <mtrmena176@gmail.com>
2025-03-26 18:16:40 +0200

    Navigate to Home View

--- lib/Features/Splash/Presentation/Views/Widgets/splash_view_body.dart
+++ lib/Features/Splash/Presentation/Views/Widgets/splash_view_body.dart
@@ -1,9 +1,26 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:bookly/Features/Home/presentation/views/home_view.dart';
 import 'package:bookly/Utils/assets.dart';
+import 'package:bookly/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Get.to(() => HomeView(),
+          transition: Transition.fadeIn, duration: kTransitionDuration);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
