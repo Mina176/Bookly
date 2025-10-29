ef6cabf34b859832ef84e9533fd9a329c79ad95c
Mina Ayman <mtrmena176@gmail.com>
2025-10-29 14:26:18 +0300

    display featured books part 1

--- lib/Features/Home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/Home/presentation/views/widgets/featured_list_view.dart
@@ -1,25 +1,38 @@
+import 'package:bookly/Core/widgets/custom_error_widget.dart';
+import 'package:bookly/Core/widgets/custom_loading_indicator.dart';
+import 'package:bookly/Features/Home/presentation/manager/featured_book_cubit/featured_books_cubit.dart';
 import 'package:bookly/Features/Home/presentation/views/widgets/featured_list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedListView extends StatelessWidget {
   const FeaturedListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 32.0),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * .3,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(right: 12.0),
-              child: FeaturedListViewItem(),
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+        builder: (context, state) {
+          if (state is FeaturedBooksSuccess) {
+            return ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 12.0),
+                  child: FeaturedListViewItem(),
+                );
+              },
             );
-          },
-        ),
+          } else if (state is FeaturedBooksFailure) {
+            return CustomErrorWidget(
+              errMessage: state.errMessage,
+            );
+          } else {
+            return CustomLoadingIndicator();
+          }
+        },
       ),
     );
   }
